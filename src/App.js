6c60340fa8d94b95b304e7d31d18f3d3c77d4798
Achraf Essaoudi <achrafess1937@gmail.com>
2022-03-20 12:21:14 +0100

    change geo api key

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-// import "leaflet/dist/leaflet.css";
-import styled from "styled-components";
 import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { useEffect } from "react";
-// import Header from "./Components/Header";
 
 import { MapContainer, TileLayer, Marker, Popup, Map } from "react-leaflet";
 import L from "leaflet";
 import { Container, Header } from "./styles/Header";
 import { Arrow, Input, InputCon } from "./styles/Input";
-import useSWR, { useSWRConfig } from "swr";
+import useSWR from "swr";
+import { fetcher } from "./Components/fetcher";
 import { useState } from "react";
 import { useRef } from "react";
 import GlobalCss from "./styles/Global.css";
 import IpInfo from "./Components/IpInfo";
 import Invalid from "./Components/Invalid";
-import { fetcher } from "./Components/fetcher";
 
 const theme = {
   main: "black",
@@ -30,23 +27,23 @@ function App() {
   const mapBoxStyleId = process.env.REACT_APP_MAPBOX_STYLEID;
   const mapBoxUsername = process.env.REACT_APP_MAPBOX_USERNAME;
   const mapBoxAccessKey = process.env.REACT_APP_MAPBOX_ACCESSKEY;
+  const geoIpAccessKey = process.env.REACT_APP_GEOIPIFY;
 
   const [url, setUrl] = useState(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_5afOeLbeIL0JLFdcniqX6pkbSzCH1&ipAddress=8.8.8.8`
+    `https://geo.ipify.org/api/v2/country,city?apiKey=${geoIpAccessKey}&ipAddress=8.8.8.8`
   );
 
+  // console.log(useSWR());
   const { data, error } = useSWR(url, fetcher);
 
   const search = () => {
     const ip = input.current.value;
     setUrl(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_5afOeLbeIL0JLFdcniqX6pkbSzCH1&ipAddress=${ip}&domain=${ip}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${geoIpAccessKey}&ipAddress=${ip}&domain=${ip}`
     );
   };
 
-  useEffect(() => {
-    // input.current.add
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -84,7 +81,8 @@ function App() {
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               // url="mapbox://styles/ashrafess010/cl0wf377l00r014qy15jm4gb0?access_token=pk.eyJ1IjoiYXNocmFmZXNzMDEwIiwiYSI6ImNsMHAyOXhwMjFlODgzY3BlZTZicXZ1NjQifQ.-Ig1ka5jNzFj2yULCcVOUg"
-              url={`https://api.mapbox.com/styles/v1/${mapBoxUsername}/${mapBoxStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token=${mapBoxAccessKey}`}
+              // url={`https://api.mapbox.com/styles/v1/${mapBoxUsername}/${mapBoxStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token=${mapBoxAccessKey}`}
+              url="https://api.mapbox.com/styles/v1/ashrafess010/cl0wfoz5n00bi14nktm96b8th/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXNocmFmZXNzMDEwIiwiYSI6ImNsMHAyOXhwMjFlODgzY3BlZTZicXZ1NjQifQ.-Ig1ka5jNzFj2yULCcVOUg"
             />
             {/* mapbox://styles/ashrafess010/cl0wfoz5n00bi14nktm96b8th */}
             <Marker
